3908bcd7dbe239d8f2f2e07b3271bb7d49661b67
jenny gomez <jenny.0411@hotmail.com>
2020-03-18 12:06:28 -0500

    New: variables, numeros, hola-mundo

---
+++ 1-Basicos/numeros.js
@@ -0,0 +1,26 @@
+/* Numeros 
+Los números son valores que se pueden usar en operaciones matemáticas
+*/
+
+console.log (1234);
+
+/* Decimales y fracciones
+JavaScript no distingue entre números enteros y decimales,
+por lo que puede usarlos juntos sin tener que convertirlos de uno a otro.
+*/
+
+console.log (10 + 3.14159);
+
+/* Las fracciones no esxisten en JavaScript,*/ 
+
+console.log (1 / 3);
+
+// Para usar números mixtos, debe sumar el número entero y la fracción.
+console.log (1 + (4 / 3));
+
+/* Números negativos */
+// Puede hacer un número negativo colocando el operador - al frente.
+console.log (-3);
+
+// También puede obtener un número negativo restando un número de un número más pequeño-
+console.log (5 - 7);
---
+++ 1-Basicos/variables.js
@@ -0,0 +1,20 @@
+// las variables son valores con nombre yque pueden almacenar cualquier tipo de vaalor en JavaScript.
+// Se llaman variables porque su valor puede variar
+// Aquí se explica cómo declarar una variable:
+// Lo declaro con el comando var
+
+var variable1 = "Sebastian es muy juicioso";
+var variable2 = "Correcto";
+var variable3 = 3;
+var variable4 = "";
+var variable5 = "sebastian";
+var variable5 = "sebastian";
+var variable6 = "Gomez";
+
+variable3 = 4;
+
+variable4 = "Giraldo"
+
+console.log (variable1, variable2, variable3, variable4);
+
+console.log ("El hijo de jenny se llama", variable5,variable4, variable6, "Y tiene", variable3, "años.");
