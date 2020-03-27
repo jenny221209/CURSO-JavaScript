ea6cf7dddcb94e20e156fc55026323280247e2f7
jenny gomez <jenny.0411@hotmail.com>
2020-03-27 17:33:33 -0500

    New: Ejercicios en intermedio de funciones suma , resta y multiplicacion

---
+++ 2-Intermedio/Ejercicios/Multiplicacion.js/js/multiplicacion.js
@@ -0,0 +1,19 @@
+// Esta linea de codigo asegura que la página esta cargada 
+$(document).on("ready", multiplicacion());
+
+function multiplicacion () {
+    // paso 1 - 2 
+    var numero1 = document.getElementById("InputNumero1").value;
+
+    var numero2 = document.getElementById("InputNumero2").value;
+
+    // paso 3
+
+    var resultado = ( parseFloat(numero1) * parseFloat(numero2) );
+
+    // paso 5
+
+    document.getElementById("LabelTotal").innerHTML = resultado
+
+    window.alert("El resultado de la multiplicacion es : " + resultado);
+}
