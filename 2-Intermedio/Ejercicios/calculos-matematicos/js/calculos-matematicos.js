e1859b818280594e803a37d006c3d8fce2451d3f
Jenny Alexandra Gomez Parra <jenny16parr@gmail.com>
2020-04-02 14:14:08 -0500

    New: ejercicio ordenador de números

--- 2-Intermedio/Ejercicios/calculos-matematicos/js/calculos-matematicos.js
+++ 2-Intermedio/Ejercicios/calculos-matematicos/js/calculos-matematicos.js
@@ -3,14 +3,18 @@ $(document).on("ready", Calculo());
 
 function Calculo() {
     
-        var eleccion = document.getElementById("selectseleccionar");
+        var eleccion = document.getElementById("selectseleccionar").value;
+        
 
         if (eleccion == "suma") {
             suma();
         } else if (eleccion == "resta") {
             resta();
+        } else if (eleccion == "multiplicar") {
+            multiplicacion();
+        } else if(eleccion == "dividir") {
+            division();
         }
-
 
     }
 
@@ -34,7 +38,44 @@ function Calculo() {
 
 
     function resta() {
-        window.alert("Hola esta en la funcio resta");
+        
+
+        var numero1 = document.getElementById("Inputnumero1").value;
+
+        var numero2 = document.getElementById("Inputnumero2").value;
+    
+        var resultado = ( ( parseInt(numero1) - parseInt(numero2) ) );
+   
+
+        window.alert("Estos son los numero que ingreso: " + numero1 + " - " + numero2 + " + " + "El resultado de la resta es:" + resultado);
+
+
+    }
+
+    function multiplicacion() {
+       
+
+        var numero1 = document.getElementById("Inputnumero1").value;
+
+        var numero2 = document.getElementById("Inputnumero2").value;
+    
+        var resultado = ( ( parseInt(numero1) * parseInt(numero2) ) );
+   
+
+        window.alert("Estos son los numero que ingreso: " + numero1 + " * " + numero2 + " + " + "El resultado de la multiplicación es:" + resultado);
+    }
+
+    function division() {
+        
+
+        var numero1 = document.getElementById("Inputnumero1").value;
+
+        var numero2 = document.getElementById("Inputnumero2").value;
+    
+        var resultado = ( ( parseInt(numero1) / parseInt(numero2) ) );
+   
+
+        window.alert("Estos son los numero que ingreso: " + numero1 + " / " + numero2 + " + " + "El resultado de la división es:" + resultado);
     }
 
   
