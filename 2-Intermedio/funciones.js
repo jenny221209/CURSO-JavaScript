3be67d4868d2f07ec87a53ce0d61ea6bab74b131
Jenny Alexandra Gomez Parra <jenny16parr@gmail.com>
2020-04-04 10:31:51 -0500

    Mod: intermedio/funciones.js

--- 2-Intermedio/funciones.js
+++ 2-Intermedio/funciones.js
@@ -1,4 +1,6 @@
+//=============================== FUNCIONES SIN PARMETROS ==========================
 
+// FUncion suma sin recibir parametros
 function suma() {
     var num1 = 5;
     var num2 = 3;
@@ -8,6 +10,7 @@ function suma() {
     console.log("El resultado de la suma: ", resultado)
 }
 
+// // FUncion resta sin recibir parametros
 function resta() {
     var num1 = 8;
     var num2 = 2;
@@ -17,4 +20,26 @@ function resta() {
     console.log("El resultado de la resta: ", resultado)
 }
 
-suma();
+//=============================== FUNCIONES CON PARAMETROS ===========================
+
+// Funcion sumadatos con parametros (num1 , num2)
+function sumaDatos ( num1, num2) {
+
+    var resultado = ( parseInt(num1) + parseInt(num2));
+    console.log("El resultado de la suma: ", resultado)
+
+}
+
+// Funcion sumaDatosNombre con parametros ( nombre, num1 , num2, num3)
+function sumaDatosNombre( nombre, num1, num2, num3) {
+
+    var resultado = ( parseInt(num1) + parseInt(num2) + parseInt(num3) );
+    console.log( "Hola",  nombre, "El resultado de la suma es: ", resultado)
+
+}
+
+//suma();
+
+//sumaDatos( 15, 15 );
+
+sumaDatosNombre ( "Jenny", 40,  60, 200);
